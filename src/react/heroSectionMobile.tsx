e57b3b25307dab2163e560d2370ca7ee7f2473ea
loqimean <vanuha277@gmail.com>
2026-03-18 21:34:26 +0100

    fix: change heading tags in Testimonials and HeroSectionMobile for semantic consistency

--- src/react/heroSectionMobile.tsx
+++ src/react/heroSectionMobile.tsx
@@ -5,7 +5,7 @@ export const HeroSectionMobile = (props: { t: any }) => {
 
   return (
     <HeroSectionShell t={t} containerClassName="flex items-center">
-      <h1 className="text-gray-200 text-[20vw] font-bold uppercase absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Welcome</h1>
+      <h2 className="text-gray-200 text-[20vw] font-bold mt-0 uppercase absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Welcome</h2>
       <img src="/stone.webp" alt="" className="w-full sm:max-h-[700px] max-h-[500px] h-full object-cover m-0!" fetchPriority="high" />
     </HeroSectionShell>
   )
